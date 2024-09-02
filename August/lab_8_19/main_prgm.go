028f399e9882e8ea54942a50f6b6b99d88d4aa17
sharansudheer <saran_sudheer@hotmail.com>
2024-09-02 23:47:37 +0530

    push:main_code
    Main Code

--- August/lab_8_19/main_prgm.go
+++ August/lab_8_19/main_prgm.go
@@ -6,15 +6,9 @@ package main
 import (
 	"fmt"
 	"lab_8_19/questions"
+	"sync"
 )
 
-func reverseString(str string) string {
-	byte_str := []rune(str)
-	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
-		byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
-	}
-	return string(byte_str)
-}
 
 func main() {
 
@@ -22,39 +16,174 @@ func main() {
 	var quest int
 	fmt.Println("Input")
 	fmt.Scanln(&quest)
-	for check == true {
+	for check {
 
 		if quest == 0 {
+
 			fmt.Println("Nice Day")
-			break
+			check = false
+
 		} else if quest> 7 {
 
 			fmt.Println("Wrong Input")
-			break
-		} else {
 
+			fmt.Println("Input")
+			fmt.Scanln(&quest)
+		
+		} else {
 			switch quest {
 
-			case 1:
-				var sentence string
-				fmt.Println("Enter Your string")
-				fmt.Scanln(&sentence)
-				fmt.Println(reverseString(sentence))
+				case 1:{
+					var sentence string
+					fmt.Println("Enter Your string")
+					fmt.Scanln(&sentence)
 
-			case 2:
-				var num1, num2 int
-				
-				fmt.Print("Enter the first number: ")
-				fmt.Scan(&num1)
+		
+					reverse_str := questions.ReverseString(sentence)
 
-				fmt.Print("Enter the second number: ")
-				fmt.Scan(&num2)
-				var question string
-				question = questions.Quest_2(8,2)
-				fmt.Print(question)
+					fmt.Println(reverse_str)
+
+					fmt.Println("Input")
+					fmt.Scanln(&quest)
+				}
+
+				case 2:{
+					var num1, num2 int
+					
+					fmt.Print("Enter the first number: ")
+					fmt.Scan(&num1)
+					fmt.Print("Enter the second number: ")
+					fmt.Scan(&num2)
+
+					questions.Quest_2(num1,num2)
+
+					fmt.Println("Input")
+					fmt.Scanln(&quest)
+				}
+
+				case 3:{
+					var rollNo int
+					fmt.Print("Enter your roll number: ")
+					fmt.Scan(&rollNo)
 				
+					questions.PrintName(rollNo)
+				
+				}
+				
+				/*
+				Question 4
+				 Returns the number of odd and even numbers in the array
+				*/
+				case 4:{
+					var n int
+					fmt.Print("Enter the size of the array: ")
+					fmt.Scan(&n)
+
+					arr := make([]int, n)
+					fmt.Println("Enter the elements of the array:")
+					for i := 0; i < n; i++ {
+						fmt.Scan(&arr[i])
+					}
+
+					
+
+					odd, even := questions.Find_odd_even(arr)
+					fmt.Printf("Number of odd numbers: %d\n", odd)
+					fmt.Printf("Number of even numbers: %d\n", even)
+
+					}
+				case 5:{
+					var n int
+					fmt.Print("Enter the size of the array: ")
+					fmt.Scan(&n)
+
+					arr := make([]int, n)
+					fmt.Println("Enter the elements of the array:")
+					for i := 0; i < n; i++ {
+						fmt.Scan(&arr[i])
+					}
+
+					var wg sync.WaitGroup
+					wg.Add(3)
+
+					go func() {
+						sum := findSum(arr)
+						fmt.Println("Sum of the array:", sum)
+						wg.Done()
+					}()
+
+					go func() {
+						average := findAverage(arr)
+						fmt.Println("Average of the array:", average)
+						wg.Done()
+					}()
+
+					go func() {
+						max := findMax(arr)
+						fmt.Println("Maximum element in the array:", max)
+						wg.Done()
+					}()
+
+					wg.Wait()
+				}
+					case 6:{
+						var n int
+						fmt.Print("Enter the size of the array: ")
+						fmt.Scan(&n)
+
+						arr := make([]int, n)
+						fmt.Println("Enter the elements of the array:")
+						for i := 0; i < n; i++ {
+							fmt.Scan(&arr[i])
+						}
+
+						var wg sync.WaitGroup
+						wg.Add(3)
+
+						go func() {
+							reverseArray(arr)
+							fmt.Println("Reversed array:", arr)
+							wg.Done()
+						}()
+
+						go func() {
+							odd, even := countOddEven(arr)
+							fmt.Printf("Number of odd numbers: %d\n", odd)
+							fmt.Printf("Number of even numbers: %d\n", even)
+							wg.Done()
+						}()
+
+						go func() {
+							zeros := countZeros(arr)
+							fmt.Printf("Number of zeros: %d\n", zeros)
+							wg.Done()
+						}()
+
+						wg.Wait()
+					}
+				case 7:{
+					var wg sync.WaitGroup
+				wg.Add(2)
+				
+				{
+
+				go func() {
+					name := Get_Name()
+					wg.Done()
+					print_data(name, Get_rollno())
+				}()
+
+				go func() {
+					rollno := Get_rollno()
+					wg.Done()
+					print_data(Get_Name(), rollno)
+				}()
+
+				wg.Wait()
+				}
 			}
 
+			}
 		}
 	}
 }
@@ -66,4 +195,4 @@ For larger projects,
 consider using Go modules to manage dependencies and package organization.
 If you want to access private functions (starting with a lowercase letter)
 from the external package, 
-you'll need to expose them through public functions or use reflection.*/
+you'll need to expose them through public functions or use reflection. */
--- August/lab_8_19/questions/quest_4.go
+++ August/lab_8_19/questions/quest_4.go
@@ -1,7 +1,5 @@
 package questions
 
-
-
 func Find_odd_even(arr []int) (int, int) {
     oddCount, evenCount := 0, 0
     for _, num := range arr {
